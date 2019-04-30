42a7d0c74d91d6aabb97bb98cd18ee51a73d06ce
Liuxq <605866812@qq.com>
2019-04-30 08:37:22 +0000

    modify page

--- resources/views/myspace.blade.php
+++ resources/views/myspace.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>Myspace</h1>
+	<h1>Myspace - 刘夏青</h1>
 	<p>Welcome to Myspace Page</p>
 	<p>Version : {{$version}}</p>
 	{{-- this is a manual --}}
